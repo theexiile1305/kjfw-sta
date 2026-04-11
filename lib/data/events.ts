6d96ed94e746544b4cfe75250c76b9fcd2f48f24
theexiile1305 <development@michifuchs.com>
2026-04-11 17:32:13 +0200

    feat: changed name of event

--- lib/data/events.ts
+++ lib/data/events.ts
@@ -10,8 +10,8 @@ export const events: CalendarEvent[] = [
     category: "competition",
   },
   {
-    id: "leistungsabzeichen-2026",
-    title: "Leistungsabzeichen Bayern 2026",
+    id: "deutschen-jugendleistungsspange-2026",
+    title: "Deutsche Jugendleistungsspange 2026",
     date: "2026-07-18",
     location: "Höhenrain",
     description: "Abnahme der Deutschen Jugendleistungsspange in Höhenrain.",
